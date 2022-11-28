9a5ed6a627b76863eb1f72688ee83b3346bf1529
midhyun <kfree912@gmail.com>
2022-11-28 23:18:12 +0900

    style : hobby_create form style, js 추가
    - 슬라이드로 구현
    - 캐러샐과 유사함
    - 스타일만 적용했음

---
+++ static/js/form.js
@@ -0,0 +1,46 @@
+const slide = document.querySelector(".slide");
+let slideWidth = slide.clientWidth;
+const prevBtn = document.querySelector(".slide_prev_button");
+const nextBtn = document.querySelector(".slide_next_button");
+const slideItems = document.querySelectorAll(".slide_item");
+const maxSlide = slideItems.length;
+let currSlide = 1;
+const pagination = document.querySelector(".slide_pagination");
+const progress = document.querySelector('#progress_bar');
+
+const paginationItems = document.querySelectorAll(".slide_pagination > li");
+console.log(paginationItems);
+
+nextBtn.addEventListener("click", () => {
+  currSlide++;
+  progress.setAttribute('style', `width:${currSlide*10}%`)
+  if (currSlide <= maxSlide) {
+    const offset = slideWidth * (currSlide - 1);
+    slideItems.forEach((i) => {
+      i.setAttribute("style", `left: ${-offset}px`);
+    });
+    // 슬라이드 이동 시 현재 활성화된 pagination 변경
+  } else {
+    currSlide--;
+  }
+});
+// 버튼 엘리먼트에 클릭 이벤트 추가하기
+prevBtn.addEventListener("click", () => {
+  // 이전 버튼 누를 경우 현재 슬라이드를 변경
+  currSlide--;
+  // 1번째 슬라이드 이하로 넘어가지 않게 하기 위해서
+  if (currSlide > 0) {
+    // 슬라이드를 이동시키기 위한 offset 계산
+    const offset = slideWidth * (currSlide - 1);
+    // 각 슬라이드 아이템의 left에 offset 적용
+    slideItems.forEach((i) => {
+      i.setAttribute("style", `left: ${-offset}px`);
+    });
+  } else {
+    currSlide++;
+  }
+});
+
+window.addEventListener("resize", () => {
+  slideWidth = slide.clientWidth;
+});
