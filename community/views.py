ba2a04f68aa2c82af9c5a1e25ab1610da9f3ca01
myeonghwna57 <myeonghwan57@gmail.com>
2022-11-29 10:25:01 +0900

    feat:community models.py index.html 작성 [김명환]

--- community/views.py
+++ community/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Community, Comment
 
 # Create your views here.
+def index(request):
+    posts = Community.objects.order_by("-pk")
+    context = {"posts": posts}
+    return render(request, "community/index.html", context)
