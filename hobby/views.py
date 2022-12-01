e926c6653abddfeed89e9e5568ae6ec8c025aceb
suyoung049 <suyoung049@gmail.com>
2022-12-01 17:23:22 +0900

    feat : Hobby/인덱스 테그추가[이수영]

--- hobby/views.py
+++ hobby/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import HobbyForm
-from .models import Hobby
+from .models import Hobby, Tag
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -24,16 +24,25 @@ def create(request):
     }
     return render(request, "hobby/form.html", context)
 
+
 def test(request):
     return render(request, "hobby/test.html")
 
+
 def index(request, category_name):
     category_posts = Hobby.objects.filter(category=category_name)
+    category_posts_hit = category_posts.order_by("-hits")[:3]
+    category_posts_new = category_posts.order_by("-created_at")[:3]
+    tags = Tag.objects.filter(category=category_name)
     context = {
         "category_name": category_name,
+        "category_posts": category_posts,
+        "category_posts_hit": category_posts_hit,
+        "category_posts_new": category_posts_new,
+        "tags": tags,
     }
     return render(request, "hobby/index.html", context)
 
 
-def tag(request, category_name, tag_name):
+def tag(request, tag_name):
     return render(request)
