5efb783ea4a0fe6dbe078b8b5b777e5afe9d50f2
jaejae200 <kj31049@naver.com>
2022-12-06 15:25:59 +0900

    chore : 유저 admin 커스텀 필드 추가[김재우]

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,3 +5,4 @@ from django.contrib.auth import get_user_model
 # Register your models here.
 
 admin.site.register(get_user_model(), UserAdmin)
+UserAdmin.fieldsets += (("Custom fields", {"fields": ("nickname", "gender", "address", "address_detail", "birth", "kakao_id", "followings", "image", "sports", "art", "travel", "food", "develop")}),)
