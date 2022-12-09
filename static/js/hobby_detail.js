4544d147e06a17b3d70390449e460da4f9681958
midhyun <kfree912@gmail.com>
2022-12-10 00:47:09 +0900

    feat : 로딩중 구현 [김현중]
    - detail page 로딩중 구현

--- static/js/hobby_detail.js
+++ static/js/hobby_detail.js
@@ -431,3 +431,24 @@ commentList.addEventListener('click', getReComment);
 commentListOff.addEventListener('click', likeComment);
 commentListOff.addEventListener('click', getDeleteComment);
 commentListOff.addEventListener('click', getReComment);
+
+// 로더
+// const loader = document.querySelector('.loader');
+// const html = document.querySelector('html');
+
+// html.style.overflow = 'hidden'; //로딩 중 스크롤 방지
+
+// window.addEventListener('load', ()=>{
+
+//      setTimeout(() => { //  <-* 로딩속도를 구현하기 위한 코드로 실제 적용시 제거
+    
+//       	loader.style.opacity = '0';
+// 		html.style.overflow = 'auto'; //스크롤 방지 해제
+      	
+//         setTimeout(() => {
+//       		loader.style.display = 'none';
+//   		}, 400);
+        
+//   }, 2000); // <-* 로딩속도 구현
+
+// })
